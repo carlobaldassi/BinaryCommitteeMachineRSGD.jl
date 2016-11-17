db405f565c32d0f56cb5354472c1690e24809f47
Carlo Baldassi <carlobaldassi@gmail.com>
2016-11-16 21:20:59 -0500

    Add extra theme package to docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,7 @@ using Documenter, BinaryCommitteeMachineRSGD
 makedocs()
 
 deploydocs(
-    deps   = Deps.pip("pygments", "mkdocs", "python-markdown-math"),
+    deps   = Deps.pip("pygments", "mkdocs", "mkdocs-bootswatch", "python-markdown-math"),
     repo   = "github.com/carlobaldassi/BinaryCommitteeMachineRSGD.jl.git",
     julia  = "0.5"
 )
